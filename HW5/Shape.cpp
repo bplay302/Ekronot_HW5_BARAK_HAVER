e93e6d997ec4b8e58636962a59914494ce431231
Cyber_User <bplay302@gmail.com>
2024-12-05 12:23:37 +0200

    feat: shape class implemented

--- HW5/Shape.cpp
+++ HW5/Shape.cpp
@@ -1,1 +1,15 @@
 #include "Shape.h"
+
+std::string Shape::getType() const
+{
+    return this->_type;
+}
+
+std::string Shape::getName() const
+{
+    return this->_name;
+}
+
+Shape::Shape(std::string name, std::string type) : _name(name), _type(type)
+{
+}
--- HW5/Shape.h
+++ HW5/Shape.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "Point.h"
-
-// Uncomment when you want to draw on Canvas and run it locally on windows
-// #include "Canvas.h" 
+#include "Canvas.h" 
 #include <string>
 
 class Shape 
@@ -12,29 +10,20 @@ public:
 	// Constructor
 	Shape(std::string name, std::string type);
 
-	// Destructor
-	~Shape();
-
 	// Getters
-	std::string getType();
-	std::string getName();
+	std::string getType() const;
+	std::string getName() const;
 
 	// Methods
-	double getArea();
-	double getPerimeter();
-	void move(Point other); // add the Point coordinates to all the points coordinates in the shape
-	void printDetails() ;
+	virtual double getArea() = 0;
+	virtual double getPerimeter() = 0;
+	virtual void move(Point other) = 0; // add the Point coordinates to all the points coordinates in the shape
+	virtual void printDetails() = 0;
 
-	//////////////////////////////////////////////////////////////////////////////
-	// Canvas and Cimg cannot be used on Linux, 								//
-	// please make sure to leave it commented if you want test to run on GitLab //
-	// You can remove comments when you run your exercise locally on Windows .  //
-	//////////////////////////////////////////////////////////////////////////////
+	virtual void draw(const Canvas& canvas) = 0;
+	virtual void clearDraw(const Canvas& canvas) = 0;
 
-	// virtual void draw(const Canvas& canvas) = 0;
-	// virtual void clearDraw(const Canvas& canvas) = 0;
-
-private:
+protected:
 	std::string _name;
 	std::string _type;
 };
